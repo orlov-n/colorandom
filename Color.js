fdbb87aaec9fb884f36b3268d0fc30fed1a91861
Neil Linton <lintonbneil@gmail.com>
2022-02-27 21:51:43 -0800

    Clean up code

--- Color.js
+++ Color.js
@@ -1,16 +1,14 @@
-//Color:
-// random hex code
-// property of locked, (boolean). Starts as unlocked.
+
 class Color {
   constructor(randomColor) {
-    this.name = randomColor; //hex code
+    this.name = randomColor; 
     this.locked = false;
   }
 
   lockColor() {
     this.locked = true;
   }
-  
+
   unlockColor() {
     this.locked = false;
   }
--- scripts.js
+++ scripts.js
@@ -1,9 +1,6 @@
 var buttonSave = document.querySelector('#save');
 var buttonNew = document.querySelector('#new');
 var paletteContainer = document.querySelector('.palette-container');
-// var imageLock = document.querySelector('.image-lock')
-// var colorBox = document.querySelector('.color-box');
-//var paletteContainer = document.querySelector('.palette-container') duplicate?
 var colorBox = document.querySelector('.color-outer-box')
 var savedPaletteSection = document.querySelector('.saved-palette-section')
 
@@ -11,23 +8,15 @@ var savedPaletteSection = document.querySelector('.saved-palette-section')
 buttonNew.addEventListener('click', makeNewPalette);
 paletteContainer.addEventListener('click', onPadlockClick);
 buttonSave.addEventListener('click', savePalette);
-
+savedPaletteSection.addEventListener('click', deletePalette)
 // Global Variables
 var palette = null;
 var savedPalettes = [];
 
-console.log('line 17', savedPalettes)
+
 window.onload = function() {
  populateColors()
-//  console.log(imageLock)
-//  console.log(savedPalettes[4].colors[4].name)
-
  renderPalette(palette)
-//  console.log('this is a palette ', palette)
-// console.log(populatedColors)
-// This is how we can lock the color of the first index
-// populatedColors.colors[0].lockColor()
-// console.log(populatedColors.colors[0])
 }
 
 function renderPalette(paletteToRender) {
@@ -63,25 +52,17 @@ function renderPalette(paletteToRender) {
 }
 
 function onPadlockClick(event) {
-  // var targetElement = event.target.closest('.color-outer-box')
   if (!event.target.dataset.colorIndex) {
    return
   }
   var selectedColorIndex = event.target.dataset.colorIndex
-  // var selectedColorIndex = targetElement.dataset.colorIndex
   if (palette.colors[selectedColorIndex].locked) {
     palette.unlockColorAtIndex(selectedColorIndex)
   }
-  //console.log(selectedColorIndex)
   else if (!palette.colors[selectedColorIndex].locked) {
     palette.lockColorAtIndex(selectedColorIndex)
-    
-    
-  //console.log('palette after: ', palette)
-  //conditional if locked...
-}
+    }
 renderPalette(palette)
-
 };
 
 function populateColors() {
@@ -92,11 +73,9 @@ function populateColors() {
   var color5 = new Color(randomHexGenerator())
   var randomColorPalette = [color1, color2, color3, color4, color5]
 
-  //console.log(persistLockedColors(randomColorPalette))
 
-  // var currentPalette =
+
   createNewPalette(randomColorPalette)
-  // return currentPalette
 }
 
 function populateNewColors() {
@@ -112,69 +91,38 @@ function populateNewColors() {
           randomColorPalette.push(color)
       }
     }
-    // var currentPalette =
     createNewPalette(randomColorPalette)
-       // return currentPalette
 }
 
 function createNewPalette(randomPalette) {
   palette = new Palette(randomPalette)
-  // console.log(palette)
   return palette
 }
 
 function makeNewPalette() {
   populateNewColors()
   renderPalette(palette)
-  // conditional to lock color in place?
 }
 
-// function savePalette(palette) {
-//   if (!savedPalettes.includes(palette)) {
-//     savedPalettes.push(palette)
-//     savedPaletteSection.innerHTML += savedPalettes
-//     console.log(savedPalettes)
-//   }
-// }
 
   function savePalette() {
-    // savedPaletteSection.innerHTML = ""
-    // console.log(savedPalettes[4].colors[4].name)
     savedPalettes.unshift(palette)
-    // console.log(savedPalettes[4].colors[4].name)
-    
-    // for (var i = 0; i < palette.colors.length; i++) {
       savedPaletteSection.innerHTML += `<section class="mini-colors-container">
       <section class="mini-1-colors mini-colors" style="background-color:${savedPalettes[0].colors[0].name}"></section>
       <section class="mini-2-colors mini-colors" style="background-color:${savedPalettes[0].colors[1].name}"></section>
       <section class="mini-3-colors mini-colors" style="background-color:${savedPalettes[0].colors[2].name}"></section>
       <section class="mini-4-colors mini-colors" style="background-color:${savedPalettes[0].colors[3].name}"></section>
       <section class="mini-5-colors mini-colors" style="background-color:${savedPalettes[0].colors[4].name}"></section>
-      <section class="delete-icon mini-colors"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <section class="delete-icon mini-colors" id="${savedPalettes[0].id}"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
     </svg></section>
       </section> `
-      // console.log(savedPalettes[4].colors[4].name)
+      populateNewColors()
     }
-    // console.log(savedPalettes[4].colors[4].name)
-  
-
-// 
 
 
-// function persistLockedColors(colors) {
-//   console.log('colors before ', colors)
-//   colors[1].lockColor() // this tests locking color at index 1
-//   for (var i = 0; i < colors.length; i++) {
-//     if (colors[i].locked === false) {
-//       colors.splice(i, 1, new Color(randomHexGenerator()))
-//       // return colors
-//     }
-//       // colors[i] = new Color(randomHexGenerator())
-//       console.log('colors after ', colors)
 
-//     }
-//   }
+
 
 function randomHexGenerator() {
   var characters = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
@@ -188,6 +136,5 @@ function randomHexGenerator() {
     randomColor += value
 
   }
-  console.log(randomColor)
   return randomColor
 }
